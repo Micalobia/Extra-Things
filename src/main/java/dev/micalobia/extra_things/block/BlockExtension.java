11e7cbde7bbbc43ccdd9f25a173f1a574627486e
Micah <micalobia2@gmail.com>
2021-08-18 04:23:09 -0700

    Added support for custom textures in BlockExtension

--- src/main/java/dev/micalobia/extra_things/block/BlockExtension.java
+++ src/main/java/dev/micalobia/extra_things/block/BlockExtension.java
@@ -24,6 +24,8 @@ import net.minecraft.util.registry.Registry;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.EnumSet;
+import java.util.HashMap;
+import java.util.Map;
 
 public class BlockExtension {
 	protected Block base;
@@ -97,39 +99,39 @@ public class BlockExtension {
 		this.renderLayer = layer;
 	}
 
-	protected void slab() {
+	protected void slab(@Nullable Identifier id) {
 		Block block = register(append("_slab"), new SlabBlock(settings));
 		if(FabricLoader.getInstance().getEnvironmentType() == EnvType.CLIENT)
 			BlockRenderLayerMap.INSTANCE.putBlock(block, renderLayer);
-		generate_slab_files();
+		generate_slab_files(id);
 	}
 
-	protected void stairs() {
+	protected void stairs(@Nullable Identifier id) {
 		Block block = register(append("_stairs"), StairsBlockFactory.create(base.getDefaultState(), settings));
 		if(FabricLoader.getInstance().getEnvironmentType() == EnvType.CLIENT)
 			BlockRenderLayerMap.INSTANCE.putBlock(block, renderLayer);
-		generate_stairs_files();
+		generate_stairs_files(id);
 	}
 
-	protected void fence() {
+	protected void fence(@Nullable Identifier id) {
 		Block block = register(append("_fence"), new FenceBlock(settings));
 		if(FabricLoader.getInstance().getEnvironmentType() == EnvType.CLIENT)
 			BlockRenderLayerMap.INSTANCE.putBlock(block, renderLayer);
-		generate_fence_files();
+		generate_fence_files(id);
 	}
 
-	protected void wall() {
+	protected void wall(@Nullable Identifier id) {
 		Block block = register(append("_wall"), new WallBlock(settings));
 		if(FabricLoader.getInstance().getEnvironmentType() == EnvType.CLIENT)
 			BlockRenderLayerMap.INSTANCE.putBlock(block, renderLayer);
-		generate_wall_files();
+		generate_wall_files(id);
 	}
 
-	protected void pane() {
+	protected void pane(@Nullable Identifier id) {
 		Block block = register(append("_pane"), PaneBlockFactory.create(settings));
 		if(FabricLoader.getInstance().getEnvironmentType() == EnvType.CLIENT)
 			BlockRenderLayerMap.INSTANCE.putBlock(block, renderLayer);
-		generate_pane_files();
+		generate_pane_files(id);
 	}
 
 	protected Identifier blockpath() {
@@ -152,6 +154,10 @@ public class BlockExtension {
 		ExtraThings.RESOURCE_PACK.addModel(JModel.modelKeepElements(parent.toString()), parrot);
 	}
 
+	protected <T> T default_if_null(@Nullable T maybeNull, T defaultValue) {
+		return maybeNull == null ? defaultValue : maybeNull;
+	}
+
 	protected void generate_base_files() {
 		JVariant variant = new JVariant();
 		variant.put("", new JBlockModel(blockpath()));
@@ -167,7 +173,7 @@ public class BlockExtension {
 		}
 	}
 
-	protected void generate_slab_files() {
+	protected void generate_slab_files(@Nullable Identifier id) {
 		JVariant variant = new JVariant();
 		variant.put("type=bottom", new JBlockModel(bappend("_slab")));
 		variant.put("type=double", new JBlockModel(blockpath()));
@@ -176,9 +182,9 @@ public class BlockExtension {
 
 		JModel template = JModel.modelKeepElements();
 		JTextures textures = JModel.textures();
-		textures.var("bottom", blockpath().toString());
-		textures.var("top", blockpath().toString());
-		textures.var("side", blockpath().toString());
+		textures.var("bottom", default_if_null(id, blockpath()).toString());
+		textures.var("top", default_if_null(id, blockpath()).toString());
+		textures.var("side", default_if_null(id, blockpath()).toString());
 		template.textures(textures);
 		ExtraThings.RESOURCE_PACK.addModel(template.clone().parent("minecraft:block/slab_top"), bappend("_slab_top"));
 		ExtraThings.RESOURCE_PACK.addModel(template.clone().parent("minecraft:block/slab"), bappend("_slab"));
@@ -188,7 +194,7 @@ public class BlockExtension {
 		}
 	}
 
-	protected void generate_stairs_files() {
+	protected void generate_stairs_files(@Nullable Identifier id) {
 		JVariant variant = new JVariant();
 		variant.put("facing=east,half=bottom,shape=inner_left", JState.model(bappend("_stairs_inner")).y(270).uvlock());
 		variant.put("facing=east,half=bottom,shape=inner_right", JState.model(bappend("_stairs_inner")));
@@ -242,9 +248,9 @@ public class BlockExtension {
 
 		JModel template = JModel.modelKeepElements();
 		JTextures textures = JModel.textures();
-		textures.var("bottom", blockpath().toString());
-		textures.var("top", blockpath().toString());
-		textures.var("side", blockpath().toString());
+		textures.var("bottom", default_if_null(id, blockpath()).toString());
+		textures.var("top", default_if_null(id, blockpath()).toString());
+		textures.var("side", default_if_null(id, blockpath()).toString());
 		template.textures(textures);
 		ExtraThings.RESOURCE_PACK.addModel(template.clone().parent("minecraft:block/stairs"), bappend("_stairs"));
 		ExtraThings.RESOURCE_PACK.addModel(template.clone().parent("minecraft:block/inner_stairs"), bappend("_stairs_inner"));
@@ -255,7 +261,7 @@ public class BlockExtension {
 		}
 	}
 
-	protected void generate_wall_files() {
+	protected void generate_wall_files(@Nullable Identifier id) {
 		JState state = JState.state();
 		state.add(JState.multipart(JState.model(bappend("_wall_post"))).when(JState.when().add("up", "true")));
 		state.add(JState.multipart(JState.model(bappend("_wall_side")).uvlock()).when(JState.when().add("north", "low")));
@@ -270,7 +276,7 @@ public class BlockExtension {
 
 		JModel template = JModel.modelKeepElements();
 		JTextures textures = JModel.textures();
-		textures.var("wall", blockpath().toString());
+		textures.var("wall", default_if_null(id, blockpath()).toString());
 		template.textures(textures);
 		ExtraThings.RESOURCE_PACK.addModel(template.clone().parent("minecraft:block/template_wall_side_tall"), bappend("_wall_side_tall"));
 		ExtraThings.RESOURCE_PACK.addModel(template.clone().parent("minecraft:block/template_wall_side"), bappend("_wall_side"));
@@ -282,7 +288,7 @@ public class BlockExtension {
 		}
 	}
 
-	protected void generate_fence_files() {
+	protected void generate_fence_files(@Nullable Identifier id) {
 		JState state = JState.state();
 		state.add(JState.multipart(JState.model(bappend("_fence_post"))));
 		state.add(JState.multipart(JState.model(bappend("_fence_side")).uvlock()).when(JState.when().add("north", "true")));
@@ -293,7 +299,7 @@ public class BlockExtension {
 
 		JModel template = JModel.modelKeepElements();
 		JTextures textures = JModel.textures();
-		textures.var("texture", blockpath().toString());
+		textures.var("texture", default_if_null(id, blockpath()).toString());
 		template.textures(textures);
 		ExtraThings.RESOURCE_PACK.addModel(template.clone().parent("minecraft:block/fence_side"), bappend("_fence_side"));
 		ExtraThings.RESOURCE_PACK.addModel(template.clone().parent("minecraft:block/fence_post"), bappend("_fence_post"));
@@ -304,7 +310,7 @@ public class BlockExtension {
 		}
 	}
 
-	protected void generate_pane_files() {
+	protected void generate_pane_files(@Nullable Identifier id) {
 		JState state = JState.state();
 		state.add(JState.multipart(JState.model(bappend("_pane_post"))));
 		state.add(JState.multipart(JState.model(bappend("_pane_side"))).when(JState.when().add("north", "true")));
@@ -319,15 +325,15 @@ public class BlockExtension {
 
 		JModel templateOne = JModel.modelKeepElements();
 		JTextures texturesOne = JModel.textures();
-		texturesOne.var("pane", blockpath().toString());
+		texturesOne.var("pane", default_if_null(id, blockpath()).toString());
 		templateOne.textures(texturesOne);
 		ExtraThings.RESOURCE_PACK.addModel(templateOne.clone().parent("minecraft:block/template_glass_pane_noside_alt"), bappend("_pane_noside_alt"));
 		ExtraThings.RESOURCE_PACK.addModel(templateOne.clone().parent("minecraft:block/template_glass_pane_noside"), bappend("_pane_noside"));
 
 		JModel templateTwo = JModel.modelKeepElements();
 		JTextures texturesTwo = JModel.textures();
-		texturesTwo.var("pane", blockpath().toString());
-		texturesTwo.var("edge", bappend("_pane_top").toString());
+		texturesTwo.var("pane", default_if_null(id, blockpath()).toString());
+		texturesTwo.var("edge", default_if_null(id, bappend("_pane_top")).toString());
 		templateTwo.textures(texturesTwo);
 		ExtraThings.RESOURCE_PACK.addModel(templateTwo.clone().parent("minecraft:block/template_glass_pane_post"), bappend("_pane_post"));
 		ExtraThings.RESOURCE_PACK.addModel(templateTwo.clone().parent("minecraft:block/template_glass_pane_side"), bappend("_pane_side"));
@@ -353,37 +359,91 @@ public class BlockExtension {
 		protected AbstractBlock.Settings blockSettings = null;
 		protected Block block = null;
 		protected Identifier blockId = null;
+		protected Map<BlockType, Identifier> customTextures;
 
 		@Environment(EnvType.CLIENT)
 		protected RenderLayer renderLayer = null;
 
 		protected Builder() {
 			blocks = EnumSet.of(BlockType.BASE);
+			customTextures = new HashMap<>();
+		}
+
+		protected Builder generic(BlockType type) {
+			blocks.add(type);
+			return this;
+		}
+
+		protected Builder generic(BlockType type, Identifier customTexture) {
+			blocks.add(type);
+			customTextures.put(type, customTexture);
+			return this;
+		}
+
+		protected Builder generic(BlockType type, String customTexture) {
+			blocks.add(type);
+			customTextures.put(type, new Identifier(customTexture));
+			return this;
 		}
 
 		public Builder slab() {
-			blocks.add(BlockType.SLAB);
-			return this;
+			return generic(BlockType.SLAB);
+		}
+
+		public Builder slab(Identifier custom_texture) {
+			return generic(BlockType.SLAB, custom_texture);
+		}
+
+		public Builder slab(String custom_texture) {
+			return generic(BlockType.SLAB, custom_texture);
 		}
 
 		public Builder stairs() {
-			blocks.add(BlockType.STAIRS);
-			return this;
+			return generic(BlockType.STAIRS);
+		}
+
+		public Builder stairs(Identifier custom_texture) {
+			return generic(BlockType.STAIRS, custom_texture);
+		}
+
+		public Builder stairs(String custom_texture) {
+			return generic(BlockType.STAIRS, custom_texture);
 		}
 
 		public Builder wall() {
-			blocks.add(BlockType.WALL);
-			return this;
+			return generic(BlockType.WALL);
+		}
+
+		public Builder wall(Identifier custom_texture) {
+			return generic(BlockType.WALL, custom_texture);
+		}
+
+		public Builder wall(String custom_texture) {
+			return generic(BlockType.WALL, custom_texture);
 		}
 
 		public Builder fence() {
-			blocks.add(BlockType.FENCE);
-			return this;
+			return generic(BlockType.FENCE);
+		}
+
+		public Builder fence(Identifier custom_texture) {
+			return generic(BlockType.FENCE, custom_texture);
+		}
+
+		public Builder fence(String custom_texture) {
+			return generic(BlockType.FENCE, custom_texture);
 		}
 
 		public Builder pane() {
-			blocks.add(BlockType.PANE);
-			return this;
+			return generic(BlockType.PANE);
+		}
+
+		public Builder pane(Identifier custom_texture) {
+			return generic(BlockType.PANE, custom_texture);
+		}
+
+		public Builder pane(String custom_texture) {
+			return generic(BlockType.PANE, custom_texture);
 		}
 
 		public Builder solid() {
@@ -442,11 +502,11 @@ public class BlockExtension {
 				be = new BlockExtension(prefix, this.block, itemSettings, this.renderLayer);
 			else
 				be = new BlockExtension(prefix, this.blockId, this.blockSettings, itemSettings, this.renderLayer);
-			if(blocks.contains(BlockType.SLAB)) be.slab();
-			if(blocks.contains(BlockType.STAIRS)) be.stairs();
-			if(blocks.contains(BlockType.WALL)) be.wall();
-			if(blocks.contains(BlockType.FENCE)) be.fence();
-			if(blocks.contains(BlockType.PANE)) be.pane();
+			if(blocks.contains(BlockType.SLAB)) be.slab(customTextures.get(BlockType.SLAB));
+			if(blocks.contains(BlockType.STAIRS)) be.stairs(customTextures.get(BlockType.STAIRS));
+			if(blocks.contains(BlockType.WALL)) be.wall(customTextures.get(BlockType.WALL));
+			if(blocks.contains(BlockType.FENCE)) be.fence(customTextures.get(BlockType.FENCE));
+			if(blocks.contains(BlockType.PANE)) be.pane(customTextures.get(BlockType.PANE));
 
 		}
 
