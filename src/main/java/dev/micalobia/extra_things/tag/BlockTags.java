7d2feddb141298dd1ca37d6697352bf48d6c84d4
Micah <micalobia2@gmail.com>
2021-08-19 09:22:37 -0700

    put all custom tags in common directory

--- src/main/java/dev/micalobia/extra_things/tag/BlockTags.java
+++ src/main/java/dev/micalobia/extra_things/tag/BlockTags.java
@@ -1,20 +1,26 @@
 package dev.micalobia.extra_things.tag;
 
-import dev.micalobia.extra_things.ExtraThings;
 import net.fabricmc.fabric.api.tag.TagRegistry;
 import net.minecraft.block.Block;
 import net.minecraft.tag.Tag;
+import net.minecraft.util.Identifier;
 
 public class BlockTags {
 	public static final Tag<Block> WARPED_NYLIUM;
 	public static final Tag<Block> CRIMSON_NYLIUM;
 	public static final Tag<Block> NETHERRACK;
 	public static final Tag<Block> IMMOVABLE;
+	public static final Tag<Block> NETHER_WART;
 
 	static {
-		WARPED_NYLIUM = TagRegistry.block(ExtraThings.id("warped_nylium"));
-		CRIMSON_NYLIUM = TagRegistry.block(ExtraThings.id("crimson_nylium"));
-		NETHERRACK = TagRegistry.block(ExtraThings.id("netherrack"));
-		IMMOVABLE = TagRegistry.block(ExtraThings.id("immovable"));
+		WARPED_NYLIUM = TagRegistry.block(common("warped_nylium"));
+		CRIMSON_NYLIUM = TagRegistry.block(common("crimson_nylium"));
+		NETHERRACK = TagRegistry.block(common("netherrack"));
+		IMMOVABLE = TagRegistry.block(common("immovable"));
+		NETHER_WART = TagRegistry.block(common("nether_wart"));
+	}
+
+	private static Identifier common(String path) {
+		return new Identifier("c", path);
 	}
 }
---
+++ src/main/java/dev/micalobia/extra_things/tag/ItemTags.java
@@ -0,0 +1,18 @@
+package dev.micalobia.extra_things.tag;
+
+import net.fabricmc.fabric.api.tag.TagRegistry;
+import net.minecraft.item.Item;
+import net.minecraft.tag.Tag;
+import net.minecraft.util.Identifier;
+
+public class ItemTags {
+	public static final Tag<Item> NETHER_WART;
+
+	static {
+		NETHER_WART = TagRegistry.item(common("nether_wart"));
+	}
+
+	private static Identifier common(String path) {
+		return new Identifier("c", path);
+	}
+}
