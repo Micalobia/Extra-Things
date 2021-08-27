2bdcc8ce5d83ed360509e67f1d9d9e3b63e6eef0
Micah <micalobia2@gmail.com>
2021-08-27 16:35:54 -0700

    Actually managed to add RecipeBookGroup/Category

--- src/main/java/dev/micalobia/extra_things/client/recipebook/ModdedRecipeBookGroup.java
+++ src/main/java/dev/micalobia/extra_things/client/recipebook/ModdedRecipeBookGroup.java
@@ -1,17 +1,16 @@
 package dev.micalobia.extra_things.client.recipebook;
 
 import com.google.common.collect.ImmutableList;
+import dev.micalobia.extra_things.ExtraThings;
 import net.minecraft.client.recipebook.RecipeBookGroup;
 
 import java.util.List;
 
 public class ModdedRecipeBookGroup {
+    public static List<RecipeBookGroup> KILN;
     public static RecipeBookGroup KILN_SEARCH;
     public static RecipeBookGroup KILN_BLOCKS;
     public static RecipeBookGroup KILN_MISC;
-
-    @SuppressWarnings("ConstantConditions")
-    public static final List<RecipeBookGroup> KILN = ImmutableList.of(KILN_SEARCH, KILN_BLOCKS, KILN_MISC);
 
     static {
         //noinspection ResultOfMethodCallIgnored
--- src/main/java/dev/micalobia/extra_things/mixin/client/recipebook/RecipeBookGroupMixin.java
+++ src/main/java/dev/micalobia/extra_things/mixin/client/recipebook/RecipeBookGroupMixin.java
@@ -1,7 +1,9 @@
 package dev.micalobia.extra_things.mixin.client.recipebook;
 
 import com.google.common.collect.ImmutableList;
-import dev.micalobia.extra_things.client.recipebook.ModdedRecipeBookGroup;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Maps;
+import dev.micalobia.extra_things.ExtraThings;
 import dev.micalobia.extra_things.recipe.book.ModdedRecipeBookCategory;
 import net.minecraft.block.Blocks;
 import net.minecraft.client.recipebook.RecipeBookGroup;
@@ -23,6 +25,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+
+import static dev.micalobia.extra_things.client.recipebook.ModdedRecipeBookGroup.*;
 
 @Mixin(RecipeBookGroup.class)
 public class RecipeBookGroupMixin {
@@ -57,13 +61,16 @@ public class RecipeBookGroupMixin {
             shift = At.Shift.AFTER
     ))
     private static void addEnum(CallbackInfo ci) {
+        ExtraThings.LOGGER.info("Settings up kiln group");
         List<RecipeBookGroup> objs = new ArrayList<>(Arrays.asList(field_1805));
         RecipeBookGroup last = objs.get(objs.size() - 1);
         int ordinal = last.ordinal();
-        ModdedRecipeBookGroup.KILN_SEARCH = add(objs, "KILN_SEARCH", ++ordinal, new ItemStack(Items.COMPASS));
-        ModdedRecipeBookGroup.KILN_BLOCKS = add(objs, "KILN_BLOCKS", ++ordinal, new ItemStack(Blocks.SAND));
-        ModdedRecipeBookGroup.KILN_MISC = add(objs, "KILN_MISC", ++ordinal, new ItemStack(Blocks.NETHERRACK));
+        KILN_SEARCH = add(objs, "KILN_SEARCH", ++ordinal, new ItemStack(Items.COMPASS));
+        KILN_BLOCKS = add(objs, "KILN_BLOCKS", ++ordinal, new ItemStack(Blocks.SAND));
+        KILN_MISC = add(objs, "KILN_MISC", ++ordinal, new ItemStack(Blocks.NETHERRACK));
         field_1805 = objs.toArray(new RecipeBookGroup[0]);
+        ExtraThings.LOGGER.info("Set up kiln group");
+        KILN = ImmutableList.of(KILN_SEARCH, KILN_BLOCKS, KILN_MISC);
     }
 
     @SuppressWarnings("UnresolvedMixinReference")
@@ -74,12 +81,14 @@ public class RecipeBookGroupMixin {
             shift = At.Shift.AFTER
     ))
     private static void addMap(CallbackInfo ci) {
-        SEARCH_MAP.put(ModdedRecipeBookGroup.KILN_SEARCH, ImmutableList.of(ModdedRecipeBookGroup.KILN_BLOCKS, ModdedRecipeBookGroup.KILN_MISC));
+        Map<RecipeBookGroup, List<RecipeBookGroup>> map = Maps.newHashMap(SEARCH_MAP);
+        map.put(KILN_SEARCH, ImmutableList.of(KILN_BLOCKS, KILN_MISC));
+        SEARCH_MAP = ImmutableMap.copyOf(map);
     }
 
     @Inject(method = "getGroups", at = @At("HEAD"), cancellable = true)
     private static void getKilnGroup(RecipeBookCategory category, CallbackInfoReturnable<List<RecipeBookGroup>> cir) {
         if (category == ModdedRecipeBookCategory.KILN)
-            cir.setReturnValue(ModdedRecipeBookGroup.KILN);
+            cir.setReturnValue(KILN);
     }
 }
